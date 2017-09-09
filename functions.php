08082e90bd915fc844887056b6c2045bb3a450f8
Arturo Cardenas <ing.arturocardenas@gmail.com>
2017-09-09 15:42:34 -0700

    editando footer functions

--- functions.php
+++ functions.php
@@ -30,11 +30,6 @@ require_once( 'inc/class-boutique-integrations.php' );
  * https://github.com/woothemes/theme-customisations
  */
 
- if ( !is_admin() ) {
-      wp_deregister_script('jquery');
-      wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"), false);
-      wp_enqueue_script('jquery');
-   }
 
    remove_action('wp_head', 'wp_generator');
    remove_action('wp_head', 'feed_links', 2);
@@ -45,11 +40,6 @@ require_once( 'inc/class-boutique-integrations.php' );
    remove_action('wp_head', 'parent_post_rel_link', 10, 0);
    remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 
-   function admin_favicon() {
-       echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('stylesheet_directory').'/images/favicon.png" />';
-   }
-   add_action('admin_head', 'admin_favicon');
-
  function mytheme_admin_bar_render() {
   global $wp_admin_bar;
   $wp_admin_bar->remove_menu('wp-logo');
@@ -57,16 +47,6 @@ require_once( 'inc/class-boutique-integrations.php' );
 add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
 
 function add_google_analytics() {
-    echo "
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-106179217-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>";
+    echo "</br><p> Escribiendo en footer</p>";
 }
 add_action('wp_footer', 'add_google_analytics');
