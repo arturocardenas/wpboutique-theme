081475bfad2744e8e84671b0872c1233e774451b
Arturo Cardenas <ing.arturocardenas@gmail.com>
2017-09-09 15:31:06 -0700

    editando functions

--- functions.php
+++ functions.php
@@ -36,10 +36,16 @@ require_once( 'inc/class-boutique-integrations.php' );
 add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
 
 function add_google_analytics() {
-    echo '<script src="http://www.google-analytics.com/ga.js" type="text/javascript"></script>';
-    echo '<script type="text/javascript">';
-    echo 'var pageTracker = _gat._getTracker("UA-106179217-1");';
-    echo 'pageTracker._trackPageview();';
-    echo '</script>';
+    echo "
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-106179217-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>";
 }
 add_action('wp_footer', 'add_google_analytics');
